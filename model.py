0435444a5f03216afb93105763c1336d0ff8a595
sriieeu <sriieeuu@gmail.com>
2025-10-28 20:26:57 +0530

    Model

--- model.py
+++ model.py
@@ -25,3 +25,41 @@ class ResidualBlock(nn.Module):
         out_add = out + shortcut
         out=torch.relu(out_add)
         return out
+    
+class AudioCNN(nn.Module):
+    def __init__(self, num_classes=50):
+        super(AudioCNN, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3,bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2,padding=1)
+        )
+        self.layer1 = nn.ModuleList([
+            ResidualBlock(64, 64) for i in range(3)
+        ])
+        self.layer2 = nn.ModuleList([ ResidualBlock(64 if i==0 else 128, 128) for i in range(4)
+        ])
+        self.layer3 = nn.ModuleList([ ResidualBlock(64 if i==0 else 256, 256) for i in range(6)
+        ])
+        self.layer4 = nn.ModuleList([ ResidualBlock(64 if i==0 else 512, 512) for i in range(3)
+        ])
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.dropout = nn.Dropout(0.5)
+        self.fc = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+       x=self.conv1(x)
+       for block in self.layer1:
+           x=block(x)
+       for block in self.layer2:
+           x=block(x)
+       for block in self.layer3:
+           x=block(x)
+       for block in self.layer4:
+           x=block(x)
+       x=self.avg_pool(x)
+       x=x.view(x.size(0), -1)
+       x=self.dropout(x)
+       x=self.fc(x)
+       return x
